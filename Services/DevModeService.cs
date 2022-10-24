b50adafba8b33df2532a704536b99521ad3fbd73
Ardo Thenardo <theogeforce@gmail.com>
2022-10-24 12:25:38 +0800

    Update BaseService and DevModeService, handle POST on add new track form, add RaceTrackEditModel

--- Services/DevModeService.cs
+++ Services/DevModeService.cs
@@ -1,3 +1,5 @@
+using System.Net.Http.Json;
+
 namespace GTRouteApp.Data;
 
 public class DevModeService: BaseService
@@ -11,6 +13,11 @@ public class DevModeService: BaseService
     // local:  $"{_baseUrl}/countries";
     // sample json: "sample-data/country.json";
     private const string GetCountryListUrl = "sample-data/country.json";
+
+    // API Url to use:
+    // local: $"{_baseUrl}/add-track";
+    // sample json: --to be created--; you can get example response, but no data will be added 
+    private const string PostNewTrackUrl = $"{_baseUrl}/add-track";
 
     private List<Country> countries = new();
     private string recentError = "";
@@ -63,6 +70,30 @@ public class DevModeService: BaseService
 
             return new List<Country>();
         }
+    }
+
+    public async Task<string> PostNewTrack(RaceTrackEditModel editModel)
+    {
+        var data = new[]
+        {
+            new KeyValuePair<string, string>("name", editModel.Name ?? ""),
+            new KeyValuePair<string, string>("country_code", editModel.CountryCode ?? ""),
+            new KeyValuePair<string, string>("logo_url", editModel.LogoUrl ?? ""),
+            new KeyValuePair<string, string>("cover_url", editModel.CoverUrl ?? ""),
+            new KeyValuePair<string, string>("category", editModel.Category ?? ""),
+            new KeyValuePair<string, string>("road_type", editModel.RoadType ?? ""),
+            new KeyValuePair<string, string>("num_of_layouts", editModel.NumberOfLayouts.ToString()),
+        };
+
+        var encodedContent = new FormUrlEncodedContent(data);
+
+        var response = await PostRequest(encodedContent, PostNewTrackUrl);
+
+        var successValue = await response.Content.ReadFromJsonAsync<RaceTrack>();
+
+        var msg = $"{successValue?.Name} ({successValue?.Category}, {successValue?.Country?.name}) was succesfully added.";
+
+        return msg;
     }
 
     public string GetRecentErrorMessage()
