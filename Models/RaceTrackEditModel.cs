1530d97922076b32ee42d728efdb52cf9387bc3d
Ardo Thenardo <50389533+ArdoThenardo@users.noreply.github.com>
2022-10-25 10:14:30 +0800

    Error Handing & Validation Improvement on Devmode - Add New Track form (#9)
    * Change form validation method & update DevModeService
    * Add validation message for add new track form, update RaceTrackEditModel and DevModeForm css
    * Remove unnecessary validation, improve validation for number of layouts, fix typo on html tag
    * Fix country list gone after other api call

--- Models/RaceTrackEditModel.cs
+++ Models/RaceTrackEditModel.cs
@@ -1,12 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GTRouteApp.Data;
 
 public class RaceTrackEditModel
 {
+    [Required(ErrorMessage = "Please enter the name of race track")]
     public string? Name { get; set; }
+
+    [Required(ErrorMessage = "Please select a country for the race track")]
     public string? CountryCode { get; set; }
+
+    [Required(ErrorMessage = "Please insert a url to logo image of race track")]
     public string? LogoUrl { get; set; }
+
+    [Required(ErrorMessage = "Please insert a url to cover image of race track")]
     public string? CoverUrl { get; set; }
+
+    [Required(ErrorMessage = "Please select a category for the race track")]
     public string? Category { get; set; }
+
+    [Required(ErrorMessage = "Please select a road type on the race track")]
     public string? RoadType { get; set; }
+
+    [Required, Range(1, 10, ErrorMessage = "Please select a number of layout (min. 1) available for race track")]
     public int NumberOfLayouts { get; set; }
 }
