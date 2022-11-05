6a4dadaa2b3684f41b313d2bc5f94057594e1c7c
Ardo Thenardo <theogeforce@gmail.com>
2022-11-05 13:12:19 +0800

    Load featured tracks from API

--- Program.cs
+++ Program.cs
@@ -9,6 +9,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddHttpClient<FeaturedService>(client => {
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+});
+
 builder.Services.AddHttpClient<RaceTrackService>(client => {
     client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
 });
