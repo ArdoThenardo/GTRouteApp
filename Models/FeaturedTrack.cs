b9a4ec3e0995c6c42a84fbb31f3accd4a337b25b
Ardo Thenardo <50389533+ArdoThenardo@users.noreply.github.com>
2022-11-05 15:16:58 +0800

    Load Featured Race Tracks on Home page (#16)
    * Subheader for Index/Home & early foundation for featured tracks
    * Layout for featured tracks in Home page
    * Load featured tracks from API
    * Add sample json for featured API

---
+++ Models/FeaturedTrack.cs
@@ -0,0 +1,11 @@
+namespace GTRouteApp.Data;
+
+public class FeaturedTrack
+{
+    public string? Slug { get; set; }
+    public string? Name { get; set; }
+    public string? CoverUrl { get; set; }
+    public string? Category { get; set; }
+    public Country? Country { get; set; }
+    public string? Text { get; set; }
+}
---
+++ Services/FeaturedService.cs
@@ -0,0 +1,43 @@
+namespace GTRouteApp.Data;
+
+public class FeaturedService: BaseService
+{
+    // API Url to use:
+    // local: $"{_baseUrl}/featured";
+    // sample json: "sample-data/featured.json";
+    private const string GetFeaturedUrl = $"{_baseUrl}/featured";
+
+    private List<FeaturedTrack> featured = new();
+    private string recentError = "";
+
+    public FeaturedService(HttpClient http): base(http) { }
+
+    public async Task<List<FeaturedTrack>> GetFeatured()
+    {
+        featured.Clear();
+        recentError = "";
+
+        try
+        {
+            var fetched = await HitRequest<BaseModel<List<FeaturedTrack>>>(GetFeaturedUrl);
+
+            if (fetched.NumberOfData == 0)
+                recentError = "There is no data";
+            
+            featured.AddRange(fetched.Data ?? new List<FeaturedTrack>());
+
+            return featured;
+        }
+        catch
+        {
+            recentError = "Unable to get data from server. Please try again at later time.";
+
+            return new List<FeaturedTrack>();
+        }
+    }
+
+    public string GetRecentErrorMessage()
+    {
+        return recentError;
+    }
+}
