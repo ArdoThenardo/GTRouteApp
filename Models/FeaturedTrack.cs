19670443f07903194ce7d4cafce0b36a2c9162e1
Ardo Thenardo <50389533+ArdoThenardo@users.noreply.github.com>
2022-11-08 00:06:57 +0800

    Visual enhancement for Featured on Home page (#17)
    * Font adjustment on featured track title & text
    * Add track logo to featured track item
    * Add reveal animation to featured track & padding adjustment on featured info
    * Add hover effect to featured track items

--- Models/FeaturedTrack.cs
+++ Models/FeaturedTrack.cs
@@ -4,6 +4,7 @@ public class FeaturedTrack
 {
     public string? Slug { get; set; }
     public string? Name { get; set; }
+    public string? LogoUrl { get; set; }
     public string? CoverUrl { get; set; }
     public string? Category { get; set; }
     public Country? Country { get; set; }
