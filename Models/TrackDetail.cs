21899a7e0e11532fc292e0a674c2c174dbcf7b71
Ardo Thenardo <theogeforce@gmail.com>
2022-11-11 13:51:13 +0800

    Access track detail page from Home & add TrackDetail model data

---
+++ Models/TrackDetail.cs
@@ -0,0 +1,10 @@
+namespace GTRouteApp.Data;
+
+public class TrackDetail
+{
+    public string? Name { get; set; }
+    public string? LogoUrl { get; set; }
+    public string? Category { get; set; }
+    public string? RoadType { get; set; }
+    public Country? Country { get; set; }
+}
