822be6447ed5fefa6c16cd6dad79fd4ceae37cdb
Ardo Thenardo <50389533+ArdoThenardo@users.noreply.github.com>
2022-11-14 19:51:05 +0800

    Implementation of Header Information on track detail page (#20)
    * Implement service to get track detail
    * Add header information and back button to Track Detail page
    * Change navigation link to track detail page & change GetDetailUrl on TrackDetailService
    * Add functionality to back button on track detail page
    * Improve back button navigation when user access Track Detail from Browse Gallery and Browse Grid

---
+++ Services/TrackDetailService.cs
@@ -0,0 +1,43 @@
+namespace GTRouteApp.Data;
+
+public class TrackDetailService: BaseService
+{
+    // API Url to use:
+    // remote: $"{_baseUrl}/{slug}";
+    // sample-json: $"sample-json/track_detail.json";
+    private const string GetDetailUrl = $"{_baseUrl}/detail/";
+
+    private TrackDetail detail = new();
+    private string recentError = "";
+
+    public TrackDetailService(HttpClient http): base(http) { }
+
+    public async Task<TrackDetail> GetTrackDetail(string slug)
+    {
+        detail = new();
+        recentError = "";
+
+        try
+        {
+            var fetched = await HitRequest<BaseModel<TrackDetail>>($"{GetDetailUrl}{slug}");
+
+            if (fetched.NumberOfData == 0 || fetched.Data == null)
+                recentError = "There is no data";
+
+            detail = fetched.Data ?? new();
+
+            return detail;
+        }
+        catch
+        {
+            recentError = "Unable to get track detail data froms server. Please try again at later time";
+
+            return new TrackDetail();
+        }
+    }
+
+    public string GetRecentErrorMessage()
+    {
+        return recentError;
+    }
+}
