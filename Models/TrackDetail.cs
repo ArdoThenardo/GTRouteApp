ef23281378de0febe71c64c8eee73056c188ee2a
Ardo Thenardo <theogeforce@gmail.com>
2022-12-02 12:42:39 +0800

    Add cover & road type information to TrackDetailHeader component

--- Models/TrackDetail.cs
+++ Models/TrackDetail.cs
@@ -3,9 +3,11 @@ namespace GTRouteApp.Data;
 public class TrackDetail
 {
     public string? Name { get; set; }
+    public string? CoverUrl { get; set; }
     public string? LogoUrl { get; set; }
     public string? Category { get; set; }
     public string? RoadType { get; set; }
+    public int NumberOfLayouts { get; set; } = 0;
     public Country? Country { get; set; }
     public List<TrackLayout> Layouts { get; set; } = new();
     public List<TrackImage> Images { get; set; } = new();
