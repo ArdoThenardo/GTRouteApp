7049e36321758e9860b796d4b3a777ae4f8c10a7
Ardo Thenardo <theogeforce@gmail.com>
2022-12-08 15:05:29 +0800

    Add New Image form to DevModeManageGallery component

---
+++ Models/GalleryEditModel.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace GTRouteApp.Data;
+
+public class GalleryEditModel
+{
+    [Required(ErrorMessage = "Please enter the name of image")]
+    public string? Name { get; set; }
+
+    [Required(ErrorMessage = "Please insert the author or source of image")]
+    public string? Author { get; set; }
+
+    [Required(ErrorMessage = "Please insert the url to image")]
+    public string? ImageUrl { get; set; }
+
+    [Required(ErrorMessage = "Please insert the description of the image")]
+    public string? Description { get; set; }
+}
