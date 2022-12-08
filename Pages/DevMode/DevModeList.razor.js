0a731ed065d10f8e608bfc97cf53ba7f97dde78a
Ardo Thenardo <theogeforce@gmail.com>
2022-12-08 09:29:51 +0800

    Refactor delete confirmation dialog code on DevModeList

--- Pages/DevMode/DevModeList.razor.js
+++ Pages/DevMode/DevModeList.razor.js
@@ -7,11 +7,3 @@ export function initializeTooltips(element) {
 export function hideTooltips() {
     itemButtonsTooltip.hide();
 }
-
-export function confirmAlert(message) {
-    if (confirm(message) == true) {
-        return true;
-    } else {
-        return false;
-    }
-}
