f6de9b0d0a5291b41109d3a4fbb368b0963b0bb5
Ardo Thenardo <50389533+ArdoThenardo@users.noreply.github.com>
2023-01-04 10:56:02 +0800

    Change Track Detail page address & adjustment for services and model (#64)
    * Adjustment for services and model to follow change from API
    * Remove unused function
    * Change address for detail page

--- Models/TrackLayout.cs
+++ Models/TrackLayout.cs
@@ -3,7 +3,7 @@ namespace GTRouteApp.Data;
 public class TrackLayout
 {
     public string? Slug { get; set; }
-    public string? Name { get; set; }
+    public string? LayoutName { get; set; }
     public string? MapUrl { get; set; }
     public double Length { get; set; }
     public int Corners { get; set; }
