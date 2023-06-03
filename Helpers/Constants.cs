5dc00a2b8e55a264c99c66a8c2bb936ac9116321
Ardo Thenardo <50389533+ArdoThenardo@users.noreply.github.com>
2023-06-03 10:09:04 +0800

    Load track information on StandalonePlayer component (#182)
    * Load track information on StandalonePlayer component
    * Access track detail page by clicking "View Track" button
    * Show NoDataView component on track information box when fetching info in progress or failed
    * Error information adjustment on track information

--- Helpers/Constants.cs
+++ Helpers/Constants.cs
@@ -34,7 +34,7 @@ public static class ErrorMessage
     public const string NoTrack = "There is no track data.";
     public const string LoadTracksFailed = "Unable to get track data from server. Please try again.";
     public const string LoadMoreTracksFailed = "There was a problem when trying to load more race tracks. Please try again.";
-    public const string LoadDetailFailed = "Unable to get track detail data froms server. Please try again.";
+    public const string LoadDetailFailed = "Unable to get track detail data. Please try again.";
     public const string LoadVideoFailed = "Unable to load video data. Please try again.";
     public const string NoRelatedVideos = "There is no other video available related to this race track.";
 }
