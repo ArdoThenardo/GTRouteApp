789b45654e87ff3de49cb598de0e08896911f848
Ardo Thenardo <50389533+ArdoThenardo@users.noreply.github.com>
2023-06-03 11:29:02 +0800

    Copywrite adjustment for some error messages (#183)

--- Helpers/Constants.cs
+++ Helpers/Constants.cs
@@ -30,9 +30,9 @@ public static class BrowseCategory
 public static class ErrorMessage
 {
     public const string NoData = "There is no data.";
-    public const string LoadDataFailed = "Unable to get data from server. Please try again.";
+    public const string LoadDataFailed = "Unable to load data. Please try again.";
     public const string NoTrack = "There is no track data.";
-    public const string LoadTracksFailed = "Unable to get track data from server. Please try again.";
+    public const string LoadTracksFailed = "Unable to get race track data. Please try again.";
     public const string LoadMoreTracksFailed = "There was a problem when trying to load more race tracks. Please try again.";
     public const string LoadDetailFailed = "Unable to get track detail data. Please try again.";
     public const string LoadVideoFailed = "Unable to load video data. Please try again.";
