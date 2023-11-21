20ded2fe4b34de8f36c513a37ce6f6c4d46c5d7b
Ardo Thenardo <50389533+ArdoThenardo@users.noreply.github.com>
2023-11-21 09:11:20 +0800

    Unit test & View adjustment for IndexFeaturedTrack component (#282)
    * View adjustment for featured tracks
    * Handle null value for featured track item
    * Create several unit tests for IndexFeaturedTrack component
    * Change unit test name
    * Change location for all files related to Index test

--- tests/Usings.cs
+++ tests/Usings.cs
@@ -1,3 +1,8 @@
 global using Xunit;
 global using Bunit;
 global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.AspNetCore.Components.Forms;
+global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.JSInterop;
+global using AngleSharp.Dom;
+global using Bunit.TestDoubles;
